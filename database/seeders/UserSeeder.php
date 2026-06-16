169d06f396d7c89f085dea2f2a773cb509e62f8b
Kannan <kannan@innopay.in>
2026-06-16 19:57:57 +0530

    Added Astrologer signups

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -31,33 +31,5 @@ class UserSeeder extends Seeder
             'mobile' => '9000000002',
             'password' => Hash::make(config('app.seed.admin_password')),
         ]);
-        Wallet::factory()->for($admin)->create(['balance' => 0]);
-
-        // Astrologers
-        $expertises = Expertise::all();
-        $languages = Language::all();
-
-        for ($i = 0; $i < 5; $i++) {
-            $user = User::factory()->astrologer()->create();
-            Wallet::factory()->for($user)->create();
-
-            $astrologer = Astrologer::factory()->approved()->create([
-                'user_id' => $user->id,
-                'consultation_modes' => [ConsultationMode::Chat->value],
-                'is_online' => $i < 3,
-            ]);
-
-            $astrologer->expertises()->attach(
-                $expertises->random(fake()->numberBetween(1, 3))->pluck('id')
-            );
-            $astrologer->languages()->attach(
-                $languages->random(fake()->numberBetween(1, 3))->pluck('id')
-            );
-        }
-
-        // Customers
-        User::factory(10)->create()->each(function (User $user) {
-            Wallet::factory()->for($user)->create();
-        });
     }
 }
